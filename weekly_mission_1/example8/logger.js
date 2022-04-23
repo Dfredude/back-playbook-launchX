aaf57715cde7810120ace48d2a8f3e44eda01ff1
Alfredo Dominguez <thecatura.ad@gmail.com>
2022-04-23 14:47:50 -0500

    add: 06, 07, 08

---
+++ weekly_mission_1/example8/logger.js
@@ -0,0 +1,17 @@
+/*
+  export default nos permite exportar esta clase e importara
+
+  import MyLogger from './logger.js'
+*/
+
+export default class Logger {
+    constructor (name) {
+      this.name = name
+    }
+  
+    log (message) {
+      console.log(`[${this.name}] ${message}`)
+    }
+  }
+
+  export const myvar = 5
