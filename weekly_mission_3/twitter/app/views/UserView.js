87bee147643e8605b0ce3cbdf28da7f05b3e3f16
Alfredo Dominguez <thecatura.ad@gmail.com>
2022-05-07 14:51:21 -0700

    add: UserView

--- weekly_mission_3/twitter/app/views/UserView.js
+++ weekly_mission_3/twitter/app/views/UserView.js
@@ -2,19 +2,22 @@ const User = require("../models/User")
 
 module.exports = class UserView{
     static createUser(attributes){
-        console.log(attributes);
         if (attributes === null || attributes === undefined){
             return {
                 error: "payload no existe"
             }
         }
         const { id, name, username, bio} = attributes
-        return new User({
+        let user = new User({
             id: id,
             name: name,
             username: username,
-            bio: bio ?? "Hey"
+            bio: bio ?? "Hey",
         })
+        user.error = typeof id != "number" || 
+        typeof name != "string" || 
+        typeof username != "string" ? "necesitan tener un valor valido" : null
 
+        return user
     }
 }
--- weekly_mission_3/twitter/test/views/UserView.test.js
+++ weekly_mission_3/twitter/test/views/UserView.test.js
@@ -5,4 +5,23 @@ describe("Tests for UserView", ()=> {
         const result =  UserView.createUser()
         expect(result.error).toMatch(/payload no existe/)
      })
+
+     test('2) Return an error when having invalid property values for the payload', ()=> {
+         const payload = { id: "id", username: null, name: 12}
+         const result = UserView.createUser(payload)
+         expect(result.error).toMatch(/necesitan tener un valor valido/)
+     })
+     
+     test('3) Return an error when having missing properties', () => {
+        const payload = { id: "id", name: 12}
+        const result = UserView.createUser(payload)
+        expect(result.error).toMatch(/necesitan tener un valor valido/)
+     })
+     test('4) Create User object by a given payload', ()=> {
+        const payload = { id: 984, username: "fredude", name: "Fred"}
+        const user = UserView.createUser(payload)
+        expect(user.name).toBe("Fred")
+        expect(user.id).toBe(984)
+        expect(user.username).toBe("fredude")
+     })
 })
