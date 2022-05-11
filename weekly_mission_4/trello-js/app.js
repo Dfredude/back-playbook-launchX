1f9914239c274ca4f910eb1dd822725143a5aa40
Alfredo Dominguez <thecatura.ad@gmail.com>
2022-05-11 15:23:14 -0700

    add: Trello API

---
+++ weekly_mission_4/trello-js/app.js
@@ -0,0 +1,20 @@
+require('dotenv').config()
+
+if(!process.env.TOKEN && !process.env.KEY){
+  throw new Error('No hay configuración con Api Key y con Token')
+}
+
+let Trello = require("trello");
+let trello = new Trello(process.env.KEY, process.env.TOKEN);
+
+let cardTitle = `Card Nueva ${new Date()}`
+
+trello.addCard(cardTitle, "LaunchX Card Description", "627c2ed3a4060639388f9c41",
+	function (error, trelloCard) {
+		if (error) {
+			console.log('Could not add card:', error);
+		}
+		else {
+			console.log('Added card:', trelloCard);
+		}
+	});
